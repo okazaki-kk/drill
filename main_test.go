3905171c70e6ebdfc1fd58f496741d5809725ae7
Kohei Okazaki <ozaki.m.mi.ys@gmail.com>
2024-03-20 10:47:07 +0900

    add dns server test

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net"
 	"os"
 	"testing"
 )
@@ -112,5 +113,65 @@ func TestResponsePacket(t *testing.T) {
 	}
 	if packet.answers[0] != expectedDnsRecords[0] {
 		t.Errorf("expected record %+v, got %+v", expectedDnsRecords[0], packet.answers[0])
+	}
+}
+
+func testServer() (*net.UDPConn, error) {
+	udpAddr, err := net.ResolveUDPAddr("udp", "8.8.8.8:53")
+	if err != nil {
+		return nil, err
+	}
+	conn, err := net.DialUDP("udp", nil, udpAddr)
+	if err != nil {
+		return nil, err
+	}
+	return conn, nil
+}
+
+func TestDNSServer(t *testing.T) {
+	conn, err := testServer()
+	if err != nil {
+		t.Errorf("error creating server: %v", err)
+	}
+	defer conn.Close()
+
+	packet := NewDnsPacket()
+	packet.header = DnsHeader{
+		id:               1234,
+		questions:        1,
+		recursionDesired: true,
+	}
+	packet.questions = []DnsQuestion{{name: "google.com", qtype: A}}
+
+	requestBuf := NewBytePacketBuffer()
+	err = packet.write(requestBuf)
+	if err != nil {
+		t.Errorf("error writing packet: %v", err)
+	}
+	_, err = conn.Write(requestBuf.buf[0:requestBuf.pos])
+	if err != nil {
+		t.Errorf("error writing request: %v", err)
+	}
+
+	responseBuf := NewBytePacketBuffer()
+	_, _, err = conn.ReadFromUDP(responseBuf.buf)
+	if err != nil {
+		t.Errorf("error reading response: %v", err)
+	}
+
+	resPacket := NewDnsPacket()
+	err = resPacket.fromBuffer(responseBuf)
+	if err != nil {
+		t.Errorf("error reading response: %v", err)
+	}
+
+	expectedDnsQuestions := []DnsQuestion{{name: "google.com", qtype: A}}
+	expectedDnsAnswersDomain := "google.com"
+
+	if resPacket.questions[0] != expectedDnsQuestions[0] {
+		t.Errorf("expected question %+v, got %+v", expectedDnsQuestions[0], resPacket.questions[0])
+	}
+	if resPacket.answers[0].domain != expectedDnsAnswersDomain {
+		t.Errorf("expected answer %+v, got %+v", expectedDnsAnswersDomain, resPacket.answers[0].domain)
 	}
 }
