b8fc7e27b1e8e9872ad47bd62d92d71c682b7368
Kohei Okazaki <ozaki.m.mi.ys@gmail.com>
2024-03-20 12:27:31 +0900

    add test

--- main_test.go
+++ main_test.go
@@ -4,10 +4,12 @@ import (
 	"net"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestQueryPacket(t *testing.T) {
-	f, err := os.Open("testdata/query_packet.bin")
+	f, err := os.Open("testdata/query_packet_google.bin")
 	if err != nil {
 		t.Fatalf("error opening file: %v", err)
 	}
@@ -57,62 +59,105 @@ func TestQueryPacket(t *testing.T) {
 }
 
 func TestResponsePacket(t *testing.T) {
-	f, err := os.Open("testdata/response_packet.bin")
-	if err != nil {
-		t.Fatalf("error opening file: %v", err)
-	}
-
-	buf := NewBytePacketBuffer()
-	_, err = f.Read(buf.buf)
-	if err != nil {
-		t.Fatalf("error reading file: %v", err)
-	}
-
-	packet := NewDnsPacket()
-	err = packet.fromBuffer(buf)
-	if err != nil {
-		t.Fatalf("error reading packet: %v", err)
-	}
-
-	expectedDnsHeader := DnsHeader{
-		id:                   12877,
-		recursionDesired:     true,
-		truncatedMessage:     false,
-		authoritativeAnswer:  false,
-		opcode:               0,
-		response:             true,
-		resCode:              NoError,
-		checkingDisabled:     false,
-		authedData:           false,
-		z:                    false,
-		recursionAvailable:   true,
-		questions:            1,
-		answers:              1,
-		authoritativeEntries: 0,
-		resourceEntries:      0,
-	}
-	expectedDnsQuestions := []DnsQuestion{
+	testcases := []struct {
+		name                 string
+		filePath             string
+		expectedDnsHeader    DnsHeader
+		expectedDnsQuestions []DnsQuestion
+		expectedDnsRecords   []DnsRecord
+	}{
 		{
-			name:  "google.com",
-			qtype: A,
+			name:     "google.com",
+			filePath: "testdata/response_packet_google.bin",
+			expectedDnsHeader: DnsHeader{
+				id:                   12877,
+				recursionDesired:     true,
+				truncatedMessage:     false,
+				authoritativeAnswer:  false,
+				opcode:               0,
+				response:             true,
+				resCode:              NoError,
+				checkingDisabled:     false,
+				authedData:           false,
+				z:                    false,
+				recursionAvailable:   true,
+				questions:            1,
+				answers:              1,
+				authoritativeEntries: 0,
+				resourceEntries:      0,
+			},
+			expectedDnsQuestions: []DnsQuestion{
+				{
+					name:  "google.com",
+					qtype: A,
+				},
+			},
+			expectedDnsRecords: []DnsRecord{
+				{
+					domain: "google.com",
+					addr:   "142.250.196.110",
+					ttl:    116,
+				},
+			},
+		},
+		{
+			name:     "yahoo.com",
+			filePath: "testdata/response_packet_yahoo.bin",
+			expectedDnsHeader: DnsHeader{
+				id:                   3035,
+				recursionDesired:     true,
+				truncatedMessage:     false,
+				authoritativeAnswer:  false,
+				opcode:               0,
+				response:             true,
+				resCode:              NoError,
+				checkingDisabled:     false,
+				authedData:           false,
+				z:                    false,
+				recursionAvailable:   true,
+				questions:            1,
+				answers:              3,
+				authoritativeEntries: 0,
+				resourceEntries:      0,
+			},
+			expectedDnsQuestions: []DnsQuestion{
+				{
+					name:  "www.yahoo.com",
+					qtype: A,
+				},
+			},
+			expectedDnsRecords: []DnsRecord{
+				{
+					domain: "me-ycpi-cf-www.g06.yahoodns.net",
+					addr:   "180.222.119.248",
+					ttl:    30,
+				},
+				{
+					domain: "me-ycpi-cf-www.g06.yahoodns.net",
+					addr:   "180.222.119.247",
+					ttl:    30,
+				},
+			},
 		},
 	}
-	expectedDnsRecords := []DnsRecord{
-		{
-			domain: "google.com",
-			addr:   "142.250.196.110",
-			ttl:    116,
-		},
-	}
 
-	if packet.header != expectedDnsHeader {
-		t.Errorf("expected header %+v, got %+v", expectedDnsHeader, packet.header)
-	}
-	if packet.questions[0] != expectedDnsQuestions[0] {
-		t.Errorf("expected question %+v, got %+v", expectedDnsQuestions[0], packet.questions[0])
-	}
-	if packet.answers[0] != expectedDnsRecords[0] {
-		t.Errorf("expected record %+v, got %+v", expectedDnsRecords[0], packet.answers[0])
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			f, err := os.Open(tc.filePath)
+			assert.NoError(t, err)
+
+			buf := NewBytePacketBuffer()
+			_, err = f.Read(buf.buf)
+			assert.NoError(t, err)
+
+			packet := NewDnsPacket()
+			err = packet.fromBuffer(buf)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tc.expectedDnsHeader, packet.header)
+			assert.ElementsMatch(t, tc.expectedDnsQuestions, packet.questions)
+			assert.ElementsMatch(t, tc.expectedDnsRecords, packet.answers)
+		})
 	}
 }
 
