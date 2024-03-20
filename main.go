6386f5d1aebbbe8685742fd0c280858931c06c61
Kohei Okazaki <ozaki.m.mi.ys@gmail.com>
2024-03-20 10:03:24 +0900

    盆栽

--- main.go
+++ main.go
@@ -11,11 +11,12 @@ func main() {
 
 	packet := NewDnsPacket()
 	packet.header.id = 1234
+	packet.header.questions = 1
 	packet.header.recursionDesired = true
 	packet.questions = append(packet.questions, DnsQuestion{name: qname, qtype: qtype})
 
-	buf := NewBytePacketBuffer()
-	err := packet.write(buf)
+	requestBuf := NewBytePacketBuffer()
+	err := packet.write(requestBuf)
 	if err != nil {
 		log.Fatalf("error writing packet: %v", err)
 	}
@@ -24,28 +25,30 @@ func main() {
 	if err != nil {
 		log.Fatalf("error resolving address: %v", err)
 	}
-
 	conn, err := net.DialUDP("udp", nil, udpAddr)
 	if err != nil {
 		log.Fatalf("error dialing: %v", err)
 	}
 	defer conn.Close()
-	conn.WriteToUDP(buf.buf[:buf.position()], udpAddr)
+	_, err = conn.WriteToUDP(requestBuf.buf[0:requestBuf.pos], udpAddr)
+	if err != nil {
+		log.Fatalf("error writing request: %v", err)
+	}
 
-	resBuffer := NewBytePacketBuffer()
-	_, _, err = conn.ReadFromUDP(resBuffer.buf)
+	responseBuf := NewBytePacketBuffer()
+	_, _, err = conn.ReadFromUDP(responseBuf.buf)
 	if err != nil {
 		log.Fatalf("error reading response: %v", err)
 	}
 
 	resPacket := NewDnsPacket()
-	err = resPacket.fromBuffer(resBuffer)
+	err = resPacket.fromBuffer(responseBuf)
 	if err != nil {
 		log.Fatalf("error reading response: %v", err)
 	}
 
 	for _, q := range resPacket.questions {
-		log.Printf("question: %s %s", q.name, q.qtype)
+		log.Printf("question: %s %v", q.name, q.qtype)
 	}
 	for _, a := range resPacket.answers {
 		log.Printf("%s: %s", a.domain, a.addr)
@@ -56,5 +59,4 @@ func main() {
 	for _, a := range resPacket.resources {
 		log.Printf("%s: %s", a.domain, a.addr)
 	}
-
 }
