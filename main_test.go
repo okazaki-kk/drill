2be6ebeae9ecd546eb5e24df721a75b77590430e
Kohei Okazaki <ozaki.m.mi.ys@gmail.com>
2024-03-24 11:57:00 +0900

    refactor test

--- main_test.go
+++ main_test.go
@@ -9,52 +9,84 @@ import (
 )
 
 func TestQueryPacket(t *testing.T) {
-	f, err := os.Open("testdata/query_packet_google.bin")
-	if err != nil {
-		t.Fatalf("error opening file: %v", err)
-	}
-
-	buf := NewBytePacketBuffer()
-	_, err = f.Read(buf.buf)
-	if err != nil {
-		t.Fatalf("error reading file: %v", err)
-	}
-
-	packet := NewDnsPacket()
-	err = packet.fromBuffer(buf)
-	if err != nil {
-		t.Fatalf("error reading packet: %v", err)
-	}
-
-	expectedDnsHeader := DnsHeader{
-		id:                   12877,
-		recursionDesired:     true,
-		truncatedMessage:     false,
-		authoritativeAnswer:  false,
-		opcode:               0,
-		response:             false,
-		resCode:              NoError,
-		checkingDisabled:     false,
-		authedData:           true,
-		z:                    false,
-		recursionAvailable:   false,
-		questions:            1,
-		answers:              0,
-		authoritativeEntries: 0,
-		resourceEntries:      0,
-	}
-	expectedDnsQuestions := []DnsQuestion{
+	testcases := []struct {
+		name                 string
+		filePath             string
+		expectedDnsHeader    DnsHeader
+		expectedDnsQuestions []DnsQuestion
+	}{
 		{
-			name:  "google.com",
-			qtype: A,
+			name:     "google.com",
+			filePath: "testdata/query_packet_google.bin",
+			expectedDnsHeader: DnsHeader{
+				id:                   12877,
+				recursionDesired:     true,
+				truncatedMessage:     false,
+				authoritativeAnswer:  false,
+				opcode:               0,
+				response:             false,
+				resCode:              NoError,
+				checkingDisabled:     false,
+				authedData:           true,
+				z:                    false,
+				recursionAvailable:   false,
+				questions:            1,
+				answers:              0,
+				authoritativeEntries: 0,
+				resourceEntries:      0,
+			},
+			expectedDnsQuestions: []DnsQuestion{
+				{
+					name:  "google.com",
+					qtype: A,
+				},
+			},
+		},
+		{
+			name:     "yahoo.com",
+			filePath: "testdata/query_packet_yahoo.bin",
+			expectedDnsHeader: DnsHeader{
+				id:                   3035,
+				recursionDesired:     true,
+				truncatedMessage:     false,
+				authoritativeAnswer:  false,
+				opcode:               0,
+				response:             false,
+				resCode:              NoError,
+				checkingDisabled:     false,
+				authedData:           true,
+				z:                    false,
+				recursionAvailable:   false,
+				questions:            1,
+				answers:              0,
+				authoritativeEntries: 0,
+				resourceEntries:      0,
+			},
+			expectedDnsQuestions: []DnsQuestion{
+				{
+					name:  "www.yahoo.com",
+					qtype: A,
+				},
+			},
 		},
 	}
 
-	if packet.header != expectedDnsHeader {
-		t.Errorf("expected header %+v, got %+v", expectedDnsHeader, packet.header)
-	}
-	if packet.questions[0] != expectedDnsQuestions[0] {
-		t.Errorf("expected question %+v, got %+v", expectedDnsQuestions[0], packet.questions[0])
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			f, err := os.Open(tc.filePath)
+			assert.NoError(t, err)
+
+			buf := NewBytePacketBuffer()
+			_, err = f.Read(buf.buf)
+			assert.NoError(t, err)
+
+			packet := NewDnsPacket()
+			err = packet.fromBuffer(buf)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tc.expectedDnsHeader, packet.header)
+			assert.Equal(t, tc.expectedDnsQuestions, packet.questions)
+		})
 	}
 }
 
@@ -134,12 +166,12 @@ func TestResponsePacket(t *testing.T) {
 				},
 				{
 					domain: "me-ycpi-cf-www.g06.yahoodns.net",
-					addr:   "180.222.119.247",
+					addr:   "180.222.119.248",
 					ttl:    30,
 				},
 				{
 					domain: "me-ycpi-cf-www.g06.yahoodns.net",
-					addr:   "180.222.119.248",
+					addr:   "180.222.119.247",
 					ttl:    30,
 				},
 			},
@@ -160,62 +192,55 @@ func TestResponsePacket(t *testing.T) {
 			assert.NoError(t, err)
 
 			assert.Equal(t, tc.expectedDnsHeader, packet.header)
-			assert.ElementsMatch(t, tc.expectedDnsQuestions, packet.questions)
-			assert.ElementsMatch(t, tc.expectedDnsRecords, packet.answers)
+			assert.Equal(t, tc.expectedDnsQuestions, packet.questions)
+			assert.Equal(t, tc.expectedDnsRecords, packet.answers)
 		})
 	}
 }
 
-func testDNSServer(t *testing.T, domainName string) {
-	udpAddr, err := net.ResolveUDPAddr("udp", "8.8.8.8:53")
-	if err != nil {
-		t.Errorf("error resolving address: %v", err)
-	}
-	conn, err := net.DialUDP("udp", nil, udpAddr)
-	if err != nil {
-		t.Errorf("error dialing: %v", err)
-	}
-	defer conn.Close()
-
-	packet := NewDnsPacket()
-	packet.header = DnsHeader{
-		id:               1234,
-		questions:        1,
-		recursionDesired: true,
-	}
-	packet.questions = []DnsQuestion{{name: domainName, qtype: A}}
-
-	requestBuf := NewBytePacketBuffer()
-	err = packet.write(requestBuf)
-	if err != nil {
-		t.Errorf("error writing packet: %v", err)
-	}
-	_, err = conn.Write(requestBuf.buf[0:requestBuf.pos])
-	if err != nil {
-		t.Errorf("error writing request: %v", err)
-	}
-
-	responseBuf := NewBytePacketBuffer()
-	_, _, err = conn.ReadFromUDP(responseBuf.buf)
-	if err != nil {
-		t.Errorf("error reading response: %v", err)
-	}
-
-	resPacket := NewDnsPacket()
-	err = resPacket.fromBuffer(responseBuf)
-	if err != nil {
-		t.Errorf("error reading response: %v", err)
-	}
-}
-
 func TestDNSServer(t *testing.T) {
-	testcases := []string{
-		"www.yahoo.com",
-		"www.google.com",
-		"www.facebook.com",
+	testcases := []struct {
+		name       string
+		domainName string
+	}{
+		{name: "yahoo", domainName: "www.yahoo.com"},
+		{name: "google", domainName: "www.google.com"},
+		{name: "facebook", domainName: "www.facebook.com"},
 	}
 
-	for _, domainName := range testcases {
-		testDNSServer(t, domainName)
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			udpAddr, err := net.ResolveUDPAddr("udp", "8.8.8.8:53")
+			if err != nil {
+				t.Errorf("error resolving address: %v", err)
+			}
+			conn, err := net.DialUDP("udp", nil, udpAddr)
+			if err != nil {
+				t.Errorf("error dialing: %v", err)
+			}
+			defer conn.Close()
+
+			packet := NewDnsPacket()
+			packet.header = DnsHeader{
+				id:               1234,
+				questions:        1,
+				recursionDesired: true,
+			}
+			packet.questions = []DnsQuestion{{name: tc.domainName, qtype: A}}
+
+			requestBuf := NewBytePacketBuffer()
+			err = packet.write(requestBuf)
+			assert.NoError(t, err)
+			_, err = conn.Write(requestBuf.buf[0:requestBuf.pos])
+			assert.NoError(t, err)
+
+			responseBuf := NewBytePacketBuffer()
+			_, _, err = conn.ReadFromUDP(responseBuf.buf)
+			assert.NoError(t, err)
+
+			resPacket := NewDnsPacket()
+			err = resPacket.fromBuffer(responseBuf)
+			assert.NoError(t, err)
+		})
 	}
 }
